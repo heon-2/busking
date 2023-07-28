02024fae7209a19eba18fb63cb1a68b5508b1764
meo-s <meoseong.ki@gmail.com>
2023-07-28 09:14:38 +0900

    refactor: CustomUserDetails의 User 도메인 모델 의존 제거

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/web/security/CustomUserDetails.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/web/security/CustomUserDetails.java
@@ -3,28 +3,27 @@ package org.comfort42.busking.web.security;
 import java.util.Collection;
 import java.util.EnumSet;
 
-import org.comfort42.busking.application.domain.model.User;
 import org.comfort42.busking.application.domain.model.UserRole;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
 
-    private String id;
+    private String username;
     private String password;
     private boolean isAccountLocked = false;
     private boolean isAccountExpired = false;
     private boolean isCredentialsExpired = false;
     private boolean isEnabled = true;
 
-    CustomUserDetails(final User user) {
-        id = user.getId();
-        password = user.getPassword();
+    CustomUserDetails(final String username, final String password) {
+        this.username = username;
+        this.password = password;
     }
 
     @Override
     public String getUsername() {
-        return id;
+        return username;
     }
 
     @Override

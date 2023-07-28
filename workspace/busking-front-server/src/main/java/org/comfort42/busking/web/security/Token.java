68a0919dd65d5acc7889efea48a818e4bf813043
meo-s <meoseong.ki@gmail.com>
2023-07-28 09:07:21 +0900

    refactor: Token을 도메인 모델로 변경

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/web/security/Token.java
+++
@@ -1,7 +0,0 @@
-package org.comfort42.busking.web.security;
-
-import java.time.Instant;
-import java.util.UUID;
-
-record Token(UUID tokenId, Instant issuedAt, String accessToken, String refreshToken) {
-}

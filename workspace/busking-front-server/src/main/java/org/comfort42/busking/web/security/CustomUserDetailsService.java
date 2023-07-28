1ee74a1dfd1f5b696dff5866d0a04ae29f078d22
meo-s <meoseong.ki@gmail.com>
2023-07-28 09:16:33 +0900

    refactor: CustomUserDetailsService에 변경사항 반영

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/web/security/CustomUserDetailsService.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/web/security/CustomUserDetailsService.java
@@ -1,12 +1,12 @@
 package org.comfort42.busking.web.security;
 
 import org.comfort42.busking.application.port.outbound.LoadUserPort;
-import org.comfort42.busking.web.security.CustomUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
+    private final static UserMapper userMapper = UserMapper.getInstacne();
 
     private final LoadUserPort loadUserPort;
 
@@ -16,9 +16,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        return new CustomUserDetails(
-                loadUserPort.loadUserById(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("")));  // TODO: 예외 메시지 작성하기
+        return userMapper.mapDomainModelToUserDetails(
+                loadUserPort
+                        .loadUserById(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("")) // TODO(meo-s): 예외 메시지 작성하기
+        );
     }
 
 }

2084bfc4224846efb1fff9da5aa7314af1b056a0
정호윤 <jhy1812@naver.com>
2023-07-31 17:19:54 +0900

    feat: 지도 페이지 지도 레이어 구현

--- workspace/frontend/src/components/Map/MapLayer.js
+++ workspace/frontend/src/components/Map/MapLayer.js
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Routes, Route, Link } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import L from 'leaflet';
+import axios from "axios";
+import { useMapStore } from '../../store.js';
+
 export function MapLayer() {
+
+    const { center, busPath, busInfo, setCenter, setBusPath, setBusInfo } = useMapStore()
+
+    console.log(center)
+    // //mount 될 때 데이터 불러오는 과정
+    useEffect(() => {
+
+    }, [])
+
+
     return (
-        <div>
-            <h1>Map Layer</h1>
+        <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+            <MapContainer
+            center={center}
+            zoom={11}
+            style={{ width: '100%', height: '1000px' }}>
+                <TileLayer 
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+            </MapContainer>
         </div>
     )
 }
--- workspace/frontend/src/store.js
+++ workspace/frontend/src/store.js
@@ -15,6 +15,9 @@ import { persist } from 'zustand/middleware';
 //     logout: () => set({ user: null }),
 // }));
 
+// User 상태
+// =======================================================
+
 let userStore = (set) => ({
     user: {
         name: null,
@@ -33,3 +36,20 @@ export const useUserStore = create(userStore)
 
 // userStore = devtools(userStore);
 // userStore = persist(userStore, { name: 'user_settings' });
+
+// Map 상태
+// =======================================================
+
+ let mapStore = (set) => ({
+    center: [26, 25],
+    busPath: [1],
+    busInfo: [1],
+    setCenter: (payload) => set({ center: payload }),
+    setBusPath: (payload) => set({ busPath: payload }),
+    setBusInfo: (payload) => set({ busInfo: payload }),
+})
+
+// mapStore = devtools(mapStore);
+// mapStore = persist(mapStore, { name: 'map_settings' });
+
+export const useMapStore = create(mapStore)

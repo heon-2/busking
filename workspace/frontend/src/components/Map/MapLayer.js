d08cf63833aae591ba97b350b45319bf71795990
정호윤 <jhy1812@naver.com>
2023-08-01 13:32:34 +0900

    feat: 버스 번호 버튼, 다이얼 메뉴 구현

--- workspace/frontend/src/components/Map/MapLayer.js
+++ workspace/frontend/src/components/Map/MapLayer.js
@@ -4,6 +4,8 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
 import L from 'leaflet';
 import axios from "axios";
 import { useMapStore } from '../../store.js';
+import { Dial } from './Dial.js';
+import { BusNum } from './BusNum.js'
 
 export function MapLayer() {
 
@@ -22,12 +24,28 @@ export function MapLayer() {
             center={center}
             zoom={11}
             style={{ width: '100%', height: '1000px' }}>
-                <TileLayer 
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
 
-            </MapContainer>
-        </div>
+무료로 사용 가능한 다양한 TileLayer 코드 목록을 아래에 제공합니다. 각 코드는 URL과 해당 타일 레이어에 대한 저작권 정보로 구성되어 있습니다.
+
+OpenStreetMap (OSM):
+jsx
+Copy code
+<TileLayer
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+/>
+Mapbox Streets:
+jsx
+Copy code
+<TileLayer
+  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+  attribution='Tiles &copy; Esri'
+  
+/>
+
+    </MapContainer>
+    <Dial />   
+    <BusNum/>  
+</div>
     )
 }

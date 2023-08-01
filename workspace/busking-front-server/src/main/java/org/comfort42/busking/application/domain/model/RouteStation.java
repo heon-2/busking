736ee0b95f3850f262e190106b86d594931d4c63
김한결 <kmr5326@naver.com>
2023-08-01 13:48:00 +0900

    refactor: RouteStation jpa entity -> domain entity로 변환

---
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/application/domain/model/RouteStation.java
@@ -0,0 +1,39 @@
+package org.comfort42.busking.application.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.Value;
+import org.comfort42.busking.persistence.adapter.outbound.RouteJpaEntity;
+import org.comfort42.busking.persistence.adapter.outbound.RouteStationJpaEntity;
+import org.comfort42.busking.persistence.adapter.outbound.StationJpaEntity;
+
+import java.util.List;
+
+@Value
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class RouteStation {
+
+
+    private RouteStationId id;
+
+    private final Station station;
+
+    private final Route route;
+
+    @Value
+    public static class RouteStationId {
+        private final Long value;
+    }
+
+    public static RouteStation withId(RouteStationId routeStationId,
+                                      Station station,
+                                      Route route
+    ) {
+
+        return new RouteStation(routeStationId,station,route);
+    }
+}

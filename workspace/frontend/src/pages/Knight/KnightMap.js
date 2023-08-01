d49431680448ba68864ebedf8351fad24b0be7b0
이지헌 <jiheon_114@naver.com>
2023-08-01 17:45:04 +0900

    feat: 기사 Quit 페이지 생성,구현

--- workspace/frontend/src/pages/Knight/KnightMap.js
+++ workspace/frontend/src/pages/Knight/KnightMap.js
@@ -1,7 +1,8 @@
 import { MapLayer } from "./../../components/Map/MapLayer";
 import { Button } from "@material-tailwind/react";
-
+import { useNavigate } from "react-router-dom";
 export function KnightMap() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>KnightMap</h1>
@@ -10,8 +11,21 @@ export function KnightMap() {
       </div>
       {/* 나중에 zindex 조정하기 */}
       <div className="flex fixed bottom-6 left-20" style={{ zIndex: 1000 }}>
-        <Button>관리자와의 화상통화 연결</Button>
-        <Button color="red">운행 종료</Button>
+        <Button
+          onClick={() => {
+            navigate("/RTC");
+          }}
+        >
+          관리자와의 화상통화 연결
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/knightquit");
+          }}
+          color="red"
+        >
+          운행 종료
+        </Button>
       </div>
     </div>
   );
--- workspace/frontend/src/pages/Knight/KnightQuit.js
+++ workspace/frontend/src/pages/Knight/KnightQuit.js
@@ -1,7 +1,27 @@
+import { Button } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
+
 export function KnightQuit() {
+  const navigate = useNavigate();
   return (
-      <div>
-          <h1>Knight Quit</h1>
-      </div>
-  )
+    <div>
+      <h1>Knight Quit</h1>
+      <p> 진짜 종료할거임 ? </p>
+      <Button
+        color="red"
+        onClick={() => {
+          navigate("/knightselect");
+        }}
+      >
+        진짜 종료할 거
+      </Button>
+      <Button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </Button>
+    </div>
+  );
 }

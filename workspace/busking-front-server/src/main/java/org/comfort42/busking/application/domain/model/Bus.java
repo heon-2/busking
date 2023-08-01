0743d7560c9c4f94a5d52a7a1dda565f0412c39a
김한결 <kmr5326@naver.com>
2023-08-01 13:51:43 +0900

    refactor: Bus jpa entity -> domain entity로 변환

---
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/application/domain/model/Bus.java
@@ -0,0 +1,34 @@
+package org.comfort42.busking.application.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.Value;
+import org.comfort42.busking.persistence.adapter.outbound.BusPK;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Value
+@RequiredArgsConstructor
+@Getter @Setter
+public class Bus {
+
+    private final BusId id;
+
+    private final Long busNum;
+
+//    private List<BusRoute> routes = new ArrayList<>();
+
+    @Value
+    public static class BusId{
+        private final BusPK value;
+    }
+
+    public static Bus withId(BusId busId,
+                             Long busNum
+    ) {
+        return new Bus(busId,busNum);
+    }
+}

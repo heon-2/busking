50c5d54ae64a4ec3f1915d57523a4fec23e2d819
Ho Yoon Jeong <jhy1812@naver.com>
2023-08-03 01:12:50 +0900

    feat:실시간 내 위치 정보 받아오는 함수 구현

--- workspace/frontend/src/common/Mylocation.js
+++ workspace/frontend/src/common/Mylocation.js
@@ -3,19 +3,27 @@ import { useGeolocated } from 'react-geolocated';
 import { useMapStore } from '../store.js';
 
 function useLocation() {
-//   const { setLocation } = useMapStore();
-//   const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
-//     positionOptions: {
-//       enableHighAccuracy: false,
-//     },
-//     userDecisionTimeout: 5000,
-//   });
+  const { setLocation } = useMapStore();
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    userDecisionTimeout: 5000,
+  });
+  console.log('뭐지')
+  console.log(coords)
+  useEffect(() => {
+    if (coords && coords.latitude && coords.longitude) {
+      setLocation([coords.latitude, coords.longitude]);
+    }
+  }, [coords]);
 
-//   useEffect(() => {
-//     if (coords && coords.latitude && coords.longitude) {
-//       setLocation([coords.latitude, coords.longitude]);
-//     }
-//   }, [coords]);
+  // 외부로 위치 정보를 반환하는 함수를 추가
+  const getLocation = () => {
+    return coords;
+  };
+
+  return { getLocation };
 }
 
 export default useLocation;

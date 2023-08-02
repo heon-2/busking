affa89dc768d917ae8f95622ee8f40c293fc61ee
meo-s <meoseong.ki@gmail.com>
2023-08-02 12:19:13 +0900

    add: CompanyMapper.mapToJpaEntity() 추가

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/CompanyMapper.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/CompanyMapper.java
@@ -1,17 +1,28 @@
 package org.comfort42.busking.persistence.adapter.outbound;
 
 import org.comfort42.busking.application.domain.model.Company;
-import org.springframework.stereotype.Component;
 
-@Component
-public class CompanyMapper {
+class CompanyMapper {
 
-    Company mapToDomainEntity(
-            CompanyJpaEntity company
-    ) {
-        return Company.withId(
-                new Company.CompanyId(company.getId()),
-                company.getName());
+    private static CompanyMapper instance = null;
+
+    public static CompanyMapper getInstance() {
+        if (instance == null) {
+            instance = new CompanyMapper();
+        }
+        return instance;
+    }
+
+    public static void destroyInstance() {
+        instance = null;
+    }
+
+    CompanyJpaEntity mapToJpaEntity(final Company company) {
+        return new CompanyJpaEntity(company.getId().value(), company.getName(), null);
+    }
+
+    Company mapToDomainEntity(final CompanyJpaEntity company) {
+        return Company.withId(new Company.CompanyId(company.getId()), company.getName());
     }
 
 }

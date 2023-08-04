12b21efbb566dd95d8e300c73b2079b7e5c6eee1
이지헌 <jiheon_114@naver.com>
2023-08-05 03:20:42 +0900

    style: 큐알 페이지에 다양한 CSS 적용 연습 & 개선 시도

--- workspace/frontend/src/components/QRcode/SelectQR.js
+++ workspace/frontend/src/components/QRcode/SelectQR.js
@@ -3,19 +3,19 @@ import { Select, Option } from "@material-tailwind/react";
 export function SelectQR() {
     return (
     <div className="flex w-72 flex-col gap-6">
-        <Select color="blue" label="탑승할 버스">
+        <Select color="blue" label="탑승할 버스" className="shadow-lg bg-white">
         <Option>1호차</Option>
         <Option>2호차</Option>
         <Option>3호차</Option>
         <Option>4호차</Option>
         </Select>
-        <Select color="purple" label="탑승할 정류장">
+        <Select color="purple" label="탑승할 정류장" className="shadow-lg bg-white">
         <Option>4-1 공항입구 GS25 앞</Option>
         <Option>4-2 운남동 에브리마트 앞</Option>
         <Option>4-3 흑석사거리</Option>
         <Option>4-4 호남파이프 건너편</Option>
         </Select>
-        <Select color="teal" label="하자지 선택">
+        <Select color="teal" label="하자지 선택" className="shadow-lg bg-white">
         <Option>SSAFY</Option>
         <Option>집으로 다시가기</Option>
         </Select>
--- workspace/frontend/src/pages/QRcode/SetQR.js
+++ workspace/frontend/src/pages/QRcode/SetQR.js
@@ -6,22 +6,30 @@ import { useNavigate } from "react-router-dom";
 export function SetQR() {
     let navigate = useNavigate();
     return (
-        <div className='bg-blue-50 bg-opacity-50'>
-            <h1>QR 생성하려는 정류장 선택 페이지</h1>
+        <div className='bg-blue-50 bg-opacity-50 h-screen'>
+            <div className="font-bold text-2xl h-20 ">
+                탑승할 버스와 하차지를 선택하세요
+            </div>
+            <div className="logo p-4 mt-10 mb-20 flex justify-center">
+            <div className="h-96 w-96 rounded-full bg-white shadow-lg flex justify-center items-center">
+            <img src="/ssabus_logo.png" alt="싸버지 로고" className='h-80'/>
 
-            <div className="logo p-4 mt-10 mb-20">
-                <img src="/ssabus_logo.png" alt="싸버지 로고" className='h-80'/>
+            </div>
+                
             </div>
 
+            <div className="flex justify-center mb-10">
             <SelectQR />
+            </div>
 
-            <div className="btngroup">
+            <div className="flex flex-col gap-5 m-16 bg-blue-50">
                 <Button onClick={()=> {
                     navigate("/scanQR");
                 }}>QR 생성</Button>
                 <Button className='bg-gray-400' onClick={()=> {
                     navigate("/map");
                 }}>홈 화면 가기</Button>
+                
             </div>
         </div>
     )

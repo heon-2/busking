9612d1816828380af827b40fa34a03232fc7ad82
기대성 <kmeos1579@gmail.com>
2023-08-04 12:08:54 +0900

    add: Company.CompanyId 기본 메서드 구현

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/application/domain/model/Company.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/application/domain/model/Company.java
@@ -13,6 +13,20 @@ public record Company(@Getter CompanyId id, @Getter String name) {
             return new CompanyId(Long.parseLong(value));
         }
 
+        @Override
+        public String toString() {
+            return Long.toString(value);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof CompanyId && ((CompanyId) obj).value == value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Long.hashCode(value);
+        }
     }
 
     public static Company withId(final CompanyId companyId, final String name) {

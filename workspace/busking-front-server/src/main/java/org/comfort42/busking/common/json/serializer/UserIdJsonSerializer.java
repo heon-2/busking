8ee9e9210c727410db378e4fd169a7b3471d0a46
기대성 <kmeos1579@gmail.com>
2023-08-04 10:03:21 +0900

    refactor: JSON 구현체 도메인 모델에서 분리

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/common/json/serializer/UserIdJsonSerializer.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/common/json/serializer/UserIdJsonSerializer.java
@@ -1,4 +1,4 @@
-package org.comfort42.busking.application.domain.serializer;
+package org.comfort42.busking.common.json.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -7,10 +7,20 @@ import org.comfort42.busking.application.domain.model.User;
 
 import java.io.IOException;
 
-public class UserIdSerializer extends JsonSerializer<User.UserId> {
+public class UserIdJsonSerializer extends JsonSerializer<User.UserId> {
+
+    @Override
+    public Class<User.UserId> handledType() {
+        return User.UserId.class;
+    }
 
     @Override
     public void serialize(User.UserId value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeString(value.toString());
+        if (value == null) {
+            gen.writeNull();
+        } else {
+            gen.writeString(value.toString());
+        }
     }
+
 }

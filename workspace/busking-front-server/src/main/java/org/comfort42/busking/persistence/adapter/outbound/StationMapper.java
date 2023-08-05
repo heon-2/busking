5cc14a30e7d13ccb5a4531991ae15c8d397e56fa
kmr5326 <kmr5326@naver.com>
2023-08-05 22:52:44 +0900

    refactor: Staion model에 Company 연관관계 추가

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/StationMapper.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/StationMapper.java
@@ -1,10 +1,15 @@
 package org.comfort42.busking.persistence.adapter.outbound;
 
+import lombok.RequiredArgsConstructor;
 import org.comfort42.busking.application.domain.model.Station;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class StationMapper {
+
+    private final CompanyMapper companyMapper;
+
     Station mapToDomainEntity(
             StationJpaEntity station
     ) {
@@ -12,7 +17,8 @@ public class StationMapper {
                 new Station.StationId(station.getId()),
                 station.getName(),
                 station.getLng(),
-                station.getLng()
+                station.getLng(),
+                companyMapper.mapToDomainEntity(station.getCompany())
         );
     }
 }

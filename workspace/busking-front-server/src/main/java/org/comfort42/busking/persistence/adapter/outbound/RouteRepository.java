f5073d41a0ffd33e5294df06853998095a811939
kmr5326 <kmr5326@naver.com>
2023-08-06 15:38:36 +0900

    refactor: JpaRepository 상속 제거

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/RouteRepository.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/RouteRepository.java
@@ -1,6 +1,22 @@
 package org.comfort42.busking.persistence.adapter.outbound;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.comfort42.busking.application.domain.model.Route;
+import org.comfort42.busking.application.port.outbound.RegisterRoutePort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RouteRepository extends JpaRepository<RouteJpaEntity,Long> {
+@Repository
+@RequiredArgsConstructor
+public class RouteRepository implements RegisterRoutePort {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Override
+    public void registerRoute(Route route) {
+
+    }
 }

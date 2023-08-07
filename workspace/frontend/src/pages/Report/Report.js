7416215c2ec8fff3e6be16dd092d61a04d564946
이지헌 <jiheon_114@naver.com>
2023-08-07 15:25:45 +0900

    feat: 사용자 신고 기능  구현

--- workspace/frontend/src/pages/Report/Report.js
+++ workspace/frontend/src/pages/Report/Report.js
@@ -11,9 +11,17 @@ import {
 import React, { useState } from "react";
 import axios from "axios";
 
+import { useUserStore, useMapStore } from "../../store";
+
 export function Report() {
   const [reportContent, setReportContent] = useState("");
-  const serverUrl = "서버주소";
+  const { location, busNum } = useUserStore();
+  // 경도
+  // const lat = location[0];
+  // // 위도
+  // const lng = location[1];
+  const accessToken = localStorage.getItem("accessToken");
+  console.log(accessToken)
 
   // 신고 항목들 변수로 담기
   const [reportList, setReportList] = useState([
@@ -23,27 +31,61 @@ export function Report() {
     "응급 상황 발생 ( 교통사고, 환자 발생 등 )",
   ]);
 
-
   function sendReport(reportContent) {
-    console.log(reportContent)
-    // post 테스트할 수 있는 사이트.
-    axios.post('https://jsonplaceholder.typicode.com/posts', {
-        reportContent : reportContent,
-    })
-    .then((response) => {
-        console.log("성공")
+    console.log(reportContent);
+    // console.log(accessToken);
+    axios
+      .post(
+        "/api/reports",
+        {
+          "description": "제대로된 테스트",
+          "lng": 1,
+          "lat": 1,
+          "busNum": 3,
+          "companyId": 1,
+        },
+
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("성공");
         // console.log(response)
         // console.log(222)
-        console.log(response.data)
+        console.log(response);
+        console.log(response.data);
         // console.log(response.data.reportContent)
         // localStorage.setItem('accessToken', response.data.accessToken)
         // setUser(response.data.user)
-    })
-    .catch((error) => {
-        console.log("되겠냐")
-        console.log(error)
-    })
-}
+      })
+      .catch((error) => {
+        console.log("되겠냐");
+        console.log(error);
+      });
+  }
+
+  function getReport() {
+    axios
+      .get("/api/reports", {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        console.log(accessToken)
+        console.log(response.data);
+        console.log('성공');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <div className="bg-gray-100">
       <div>
@@ -74,7 +116,6 @@ export function Report() {
                           className: "p-0",
                         }}
                       />
-              
                     </ListItemPrefix>
                     <Typography color="blue-gray" className="font-medium">
                       {/* 신고 내용 각각을 list라는 이름으로 출력함 */}
@@ -94,9 +135,16 @@ export function Report() {
         >
           신 고 하 기
         </Button>
+
+        <Button
+          onClick={() => {
+            getReport();
+          }}
+        >
+          {" "}
+          신고조회해보자
+        </Button>
       </div>
     </div>
   );
 }
-
-

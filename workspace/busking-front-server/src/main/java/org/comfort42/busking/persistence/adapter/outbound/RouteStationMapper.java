4e1039e1b6e18570d7dbdcddf7f353d85a8cea5a
김한결 <kmr5326@naver.com>
2023-08-07 14:07:20 +0900

    feat: mapToJpaEntitiy() domain -> Jpa 모델 매핑 함수 구현

--- workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/RouteStationMapper.java
+++ workspace/busking-front-server/src/main/java/org/comfort42/busking/persistence/adapter/outbound/RouteStationMapper.java
@@ -5,21 +5,39 @@ import org.comfort42.busking.application.domain.model.RouteStation;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class RouteStationMapper {
-    private final StationMapper stationMapper;
-    private final RouteMapper routeMapper;
+
+    private static RouteStationMapper instance=null;
+
+    public static RouteStationMapper getInstance() {
+        if (instance == null) {
+            instance = new RouteStationMapper();
+        }
+        return instance;
+    }
+
+
+    private StationMapper stationMapper=new StationMapper();
+    private RouteMapper routeMapper=new RouteMapper();
 
     public RouteStation mapToDomainEntity(
-            RouteStationJpaEntity routeStationJpaEntity,
-            StationJpaEntity station,
-            RouteJpaEntity route
+            RouteStationJpaEntity routeStationJpaEntity
     ){
         return RouteStation.withId(
                 new RouteStation.RouteStationId(routeStationJpaEntity.getId()),
-                stationMapper.mapToDomainEntity(station),
-                routeMapper.mapToDomainEntity(route)
+                stationMapper.mapToDomainEntity(routeStationJpaEntity.getStation()),
+                routeMapper.mapToDomainEntity(routeStationJpaEntity.getRoute())
                 );
+    }
+
+    public RouteStationJpaEntity mapToJpaEntity(
+            RouteStation routeStation
+    ){
+        return new RouteStationJpaEntity(
+                routeStation.getId().getValue(),
+                stationMapper.mapToJpaEntity(routeStation.getStation()),
+                routeMapper.mapToJpaEntity(routeStation.getRoute())
+        );
     }
 
 }

64fcae92c2b8aafeead9bcc68a3483da2323e724
이지헌 <jiheon_114@naver.com>
2023-08-07 15:26:39 +0900

    feat: 신고 목록 페이지 구현

--- workspace/frontend/src/components/Admin/ReportList.js
+++ workspace/frontend/src/components/Admin/ReportList.js
@@ -1,7 +1,125 @@
+import React from "react";
+import { Card, Typography, Button } from "@material-tailwind/react";
+import axios from "axios";
+
 export function ReportList() {
+  // 토큰 불러와서
+  const accessToken = localStorage.getItem("accessToken");
+  // 정보 조회하는 함수
+  function getReport() {
+    axios
+      .get("/api/reports", {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        // console.log(accessToken)
+
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  const TABLE_HEAD = ["신고 번호", "신고자", "버스 호차", "신고 내용"];
+
+  const TABLE_ROWS = [
+    {
+      name: "정호윤",
+      role: "Manager",
+      mobile: "010-1234-5678",
+    },
+    {
+      name: "이지헌",
+      role: "Developer",
+      mobile: "010-1234-5678",
+    },
+  ];
+
   return (
-      <div>
-          <h1>ReportList</h1>
-      </div>
-  )
+    <div>
+      <Button onClick={() => getReport()}>정보조회</Button>
+      <Card className="h-full w-full overflow-scroll mt-4">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {TABLE_ROWS.map(({ name, role, mobile }, index) => (
+              <tr key={name} className="even:bg-blue-gray-50/50">
+                <td className="p-4">
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {name}
+                  </Typography>
+                </td>
+                <td className="p-4">
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {role}
+                  </Typography>
+                </td>
+                <td className="p-4">
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {mobile}
+                  </Typography>
+                </td>
+                <td className="p-4">
+                  <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue"
+                    className="font-medium"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                  </Typography>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Card>
+    </div>
+  );
 }

4b17affb21fffc5603dd5c8c6c48eeae15a92f94
Ho Yoon Jeong <jhy1812@naver.com>
2023-08-10 21:38:02 +0900

    fix:three.js 삭제

--- workspace/frontend/src/pages/Auth/Login.js
+++ workspace/frontend/src/pages/Auth/Login.js
@@ -1,22 +1,13 @@
 import { LoginForm } from "../../components/Auth/LoginForm";
-import { Scene } from "../../components/Scene";
 
 export function Login() {
     return (
         <div >
         <div className="">
-            <div className="bg-black" style={{ overflow: 'hidden', position: 'relative' }}>
-            <Scene />
-            </div>
+            
             <div className="">
             </div>
-            <div className="main">
-                <div className="loginspace">
-                    <div className="login-container">
             <LoginForm />
-                    </div>
-                </div>
-            </div>
         </div>
         </div>
     )

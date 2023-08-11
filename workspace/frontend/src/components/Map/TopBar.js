9d5ed71f374b16dc85d4f20c0b0b86cfe3d31c23
이지헌 <jiheon_114@naver.com>
2023-08-12 00:12:40 +0900

    style: TopBar, LoginForm CSS 적용

--- workspace/frontend/src/components/Map/TopBar.js
+++ workspace/frontend/src/components/Map/TopBar.js
@@ -8,13 +8,13 @@ export function TopBar(props) {
   const navigate = useNavigate();
   return (
     <div className="flex justify-center">
-      <Navbar className="w-screen h-[7vh] px-3 py-[0.7vh]">
+      <Navbar className="w-screen h-12 px-3 py-[0.7vh]">
         {/* <Navbar className="w-[90vw] h-[8vh] mt-3"> */}
         <div className="flex justify-between">
-          <div className="flex items-start gap-2">
+          <div className="flex items-center gap-2">
             <div>
               <img
-                className="h-10 w-10 rounded-none flex items-center justify-center object-cover relative !rounded-full border-2 border-blue-500"
+                className=" h-8 w-8 rounded-none flex items-center justify-center object-cover relative !rounded-full border-2 border-blue-500"
                 src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
                 alt="avatar"
                 // withBorder={true}
@@ -41,7 +41,7 @@ export function TopBar(props) {
           <div className="flex items-center">
             <div>
               <img
-                className="h-8 w-8 rounded-none flex items-center justify-center relative"
+                className="h-6 w-6 mx-2 rounded-none flex items-center justify-center relative"
                 onClick={() => navigate("/report")}
                 src="/reportButton.png"
                 alt="avatar"

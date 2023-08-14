54df4222e95a65ce6592b59d7f4dd8cde81e545b
이지헌 <jiheon_114@naver.com>
2023-08-14 21:21:44 +0900

    feat: 실시간 송수신 구현 중

--- workspace/frontend/public/firebase-messaging-sw.js
+++ workspace/frontend/public/firebase-messaging-sw.js
@@ -12,11 +12,11 @@ self.addEventListener("push", function (e) {
   if (!e.data.json()) return;
 
   const resultData = e.data.json().notification;
-  const notificationTitle = '긴급 상황 발생';
+  const notificationTitle = "긴급 상황 발생";
   const notificationOptions = {
-    body: '버스 기사님이 화상 통화를 요청하셨습니다.',
-    icon: 'bus_512.png',
-    image : 'alarm.png',
+    body: "버스 기사님이 화상 통화를 요청하셨습니다.",
+    icon: "bus_512.png",
+    image: "alarm.png",
     tag: resultData.tag,
     ...resultData,
   };

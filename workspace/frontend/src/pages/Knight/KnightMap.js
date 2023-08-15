3812ae8c6a3fde730b53659fac1189b498f2c907
이지헌 <jiheon_114@naver.com>
2023-08-16 00:39:07 +0900

    feat: PWA 기능 구현을 위한 초기 배율 수정

--- workspace/frontend/src/pages/Knight/KnightMap.js
+++ workspace/frontend/src/pages/Knight/KnightMap.js
@@ -103,7 +103,7 @@ export function KnightMap() {
       </div>
       {/* 나중에 zindex 조정하기 */}
       <div className="flex fixed bottom-10 left-10" style={{ zIndex: 1000 }}>
-        {/* <RTC></RTC> */}
+        <RTC></RTC>
       </div>
       <div className="flex fixed bottom-10 right-10" style={{ zIndex: 1000 }}>
         <Button
